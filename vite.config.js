7d9d8e03e5b4b5c92c899d9c7c4f11d2c1f89080
VeronicaOtherWorld <aisileyi@gmail.com>
2025-02-24 00:13:54 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/google-map/",
+  build: {
+    outDir: "dist", // 确保输出的目录是 dist
+  },
   plugins: [react()],
   server: {
     headers: {
